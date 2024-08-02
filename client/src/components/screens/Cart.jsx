5ce4c1b546e32f1467619fe23a14affcc9917688
Shaik Ibrahim <110925050+ibrahim-tech07@users.noreply.github.com>
2024-08-02 11:44:09 +0530

    Update Cart.jsx

--- client/src/components/screens/Cart.jsx
+++ client/src/components/screens/Cart.jsx
@@ -18,7 +18,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch("https://quickbite-bavkend.onrender.com/api/orderData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: "POST",
