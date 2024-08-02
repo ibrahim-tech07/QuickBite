b0bdf96fb1892754a1a06a540a41ab0c56304f93
Shaik Ibrahim <110925050+ibrahim-tech07@users.noreply.github.com>
2024-08-02 11:36:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,32 +1,33 @@
 import express from 'express';
-import mongoDB from './db.js'; // Correctly import the mongoDB function
+import mongoDB from './db.js';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.js';
 import fetchRoute from "./routes/fetchData.js";
-import orderRoute from "./routes/OrderData.js"
+import orderRoute from "./routes/OrderData.js";
 import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Fallback to port 5000 if PORT is not defined
+const PORT = process.env.PORT || 5000;
 
 // Initialize the MongoDB connection
-mongoDB();
+mongoDB().then(() => {
+    console.log("Connected to MongoDB");
+}).catch(error => {
+    console.error("MongoDB connection error:", error);
+});
 
 // Enable CORS for specific origin
 app.use(cors({
-    origin: 'https://quickbite-frontend-z2ew.onrender.com'
+    origin: 'https://quickbite-frontend-z2ew.onrender.com',
+    credentials: true
 }));
 
 app.use(express.json());
 app.use('/api', userRoute);
 app.use("/api", fetchRoute);
 app.use("/api", orderRoute);
-
-app.get('/', (req, res) => {
-    res.send('helloworld');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
