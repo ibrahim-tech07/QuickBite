f65ff7d7838dd66083dc884c1e50025a3ca4ec56
Shaik Ibrahim <110925050+ibrahim-tech07@users.noreply.github.com>
2024-08-02 11:02:27 +0530

    Update Login.jsx

--- client/src/components/screens/Login.jsx
+++ client/src/components/screens/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("https://quickbite-bavkend.onrender.com/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
